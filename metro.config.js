fdd797e08280a6b20e68633ce50f422f24ae68c1
Jakub Gladysz <jakub.gladysz@protonmail.com>
2025-06-02 21:08:43 +0300

    upgrade versions for expo

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,7 @@ const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
+/** @type {import('expo/metro-config').MetroConfig} */
+config.resolver.unstable_enablePackageExports = false
+
 module.exports = withNativeWind(config, { input: "./global.css" });
